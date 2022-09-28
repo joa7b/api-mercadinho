f49735ae5e6485847dafbb792a97bcf3cfa7b5df
joa7b <joa7@protonmail.com>
2022-09-28 15:35:42 -0300

    create user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,33 @@
 const userService = require('user.service');
 
-const createUserController
+const createUserController = async (req, res) => {
+  const { username, name, email, password, avatar } = req.body;
+
+  if (!username || !name || !email || !password || !avatar) {
+    return res.status(400).send({
+      message: 'Alguns campos estão faltando.',
+    });
+  }
+
+  const foundUser = await userService.findByEmailUserService(email);
+
+  if (foundUser) {
+    return res.status(400).send({
+      message: 'Usuário já cadastrado.',
+    });
+  }
+
+  const user = await userService
+    .createUserService(req.body)
+    .catch((err) => console.log(err, message));
+
+  if (!user) {
+    return res.status(400).send({
+      message: 'Erro ao cadastrar o usuário.',
+    });
+  }
+
+  res.status(201).send(user);
+};
+
+module.exports = { createUserController };
