2021da9ae1cf10729c83a40e15baae8164cec655
joa7b <joa7@protonmail.com>
2022-10-11 20:37:44 -0300

    edit produto schema

--- src/models/Produto.js
+++ src/models/Produto.js
@@ -4,7 +4,8 @@ const ProdutoSchema = new mongooose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  quantity: { type: Number, required: true },
+  image: { type: String, required: true },
+  measure: { type: String, required: true },
 });
 
 const Produto = mongooose.model('mercadinho-api.produtos', ProdutoSchema);
