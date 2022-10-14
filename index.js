db1715cdb3957cc8c55a77be922f28a811e2cfbd
joa7b <joa7@protonmail.com>
2022-10-14 14:50:37 -0300

    create user route and formating code with prettier

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const routesProdutos = require('./src/routes/produto.route');
+const routesUser = require('./src/routes/user.route');
 const connectToDatabase = require('./src/database/database');
 
 const port = process.env.PORT || 3001;
@@ -12,6 +13,7 @@ connectToDatabase();
 app.use(express.json());
 app.use(cors());
 app.use('/', routesProdutos);
+app.use('/users', routesUser);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
